f83b691a0e371f4294fe7db2575eaa0d4bad9250
coolseaweed <jeellato@gmail.com>
2024-02-23 13:26:08 +0000

    update

--- Language/Python/GIL/gil_test.py
+++ Language/Python/GIL/gil_test.py
@@ -1,37 +1,64 @@
-from time import time
+from time import perf_counter, sleep
 import threading
 
 
-def loop(num=50000000):
+def cpu_bound_task(num_jobs):
+    x = 0
+    for i in range(num_jobs):
+        x += 1
 
-    for i in range(num):
-        pass
+
+def io_bound_task(sleep_time):
+    sleep(sleep_time)
 
 
-def test_cpu_bound(num=50000000):
+def test_io_bound(num_thread, sleep_time=8):
 
-    # single thread
-    start = time()
-    loop(num)
-    loop(num)
-    st_time = time() - start
+    threads = []
 
-    # multi thread
-    start = time()
-    thread1 = threading.Thread(target=loop, args=(num,))
-    thread2 = threading.Thread(target=loop, args=(num,))
-    thread1.start()
-    thread2.start()
-    thread1.join()
-    thread2.join()
-    mt_time = time() - start
+    start = perf_counter()
+    for _ in range(num_thread):
+        t = threading.Thread(target=io_bound_task, args=(sleep_time // num_thread,))
+        t.start()
+        threads.append(t)
 
-    report = f"---- CPU BOUND TEST ----\n* single thread runtime: {st_time:.4f} sec\n* multi thread runtime: {mt_time:.4f} sec\n"
-    print(report)
+    for t in threads:
+        t.join()
+    runtime = perf_counter() - start
+
+    print(f"* [{num_thread}] thread runtime: {runtime:.2f} sec")
+
+
+def test_cpu_bound(num_thread, num_jobs=1000000):
+
+    threads = []
+
+    start = perf_counter()
+    for _ in range(num_thread):
+        t = threading.Thread(target=cpu_bound_task, args=(num_jobs // num_thread,))
+        t.start()
+        threads.append(t)
+
+    for t in threads:
+        t.join()
+    runtime = perf_counter() - start
+
+    print(f"* [{num_thread}] thread runtime: {runtime:.2f} sec")
 
 
 def main():
-    test_cpu_bound()
+
+    test_threads = [1, 2, 4, 8]
+
+    print(f"---- START CPU BOUND TEST ----")
+    for num_thread in test_threads:
+        test_cpu_bound(num_thread)
+    print(f"---- END TEST ----\n")
+
+    print(f"---- START I/O BOUND TEST ----")
+    for num_thread in test_threads:
+        test_io_bound(num_thread)
+    print(f"---- END TEST ----")
 
 
 if __name__ == "__main__":
