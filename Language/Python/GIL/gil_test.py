d179797ed7c4ced0508756482beafc9bb94fd829
coolseaweed <jeellato@gmail.com>
2024-02-23 13:38:37 +0000

    update

--- Language/Python/GIL/gil_test.py
+++ Language/Python/GIL/gil_test.py
@@ -1,5 +1,6 @@
 from time import perf_counter, sleep
 import threading
+import multiprocessing
 
 
 def cpu_bound_task(num_jobs):
@@ -12,13 +13,13 @@ def io_bound_task(sleep_time):
     sleep(sleep_time)
 
 
-def test_io_bound(num_thread, sleep_time=8):
+def test_io_bound_thread(num_thread, sleep_time=4):
 
     threads = []
 
     start = perf_counter()
     for _ in range(num_thread):
-        t = threading.Thread(target=io_bound_task, args=(sleep_time // num_thread,))
+        t = threading.Thread(target=io_bound_task, args=(sleep_time / num_thread,))
         t.start()
         threads.append(t)
 
@@ -26,10 +27,10 @@ def test_io_bound(num_thread, sleep_time=8):
         t.join()
     runtime = perf_counter() - start
 
-    print(f"* [{num_thread}] thread runtime: {runtime:.2f} sec")
+    print(f"* [{num_thread}] thread runtime: {runtime:.4f} sec")
 
 
-def test_cpu_bound(num_thread, num_jobs=1000000):
+def test_cpu_bound_thread(num_thread, num_jobs=1000000):
 
     threads = []
 
@@ -43,22 +44,65 @@ def test_cpu_bound(num_thread, num_jobs=1000000):
         t.join()
     runtime = perf_counter() - start
 
-    print(f"* [{num_thread}] thread runtime: {runtime:.2f} sec")
+    print(f"* [{num_thread}] thread runtime: {runtime:.4f} sec")
+
+
+def test_io_bound_process(num_proc, sleep_time=4):
+
+    procs = []
+
+    start = perf_counter()
+    for _ in range(num_proc):
+        p = multiprocessing.Process(target=io_bound_task, args=(sleep_time / num_proc,))
+        p.start()
+        procs.append(p)
+
+    for p in procs:
+        p.join()
+    runtime = perf_counter() - start
+
+    print(f"* [{num_proc}] thread runtime: {runtime:.4f} sec")
+
+
+def test_cpu_bound_process(num_proc, num_jobs=1000000):
+
+    procs = []
+
+    start = perf_counter()
+    for _ in range(num_proc):
+        p = multiprocessing.Process(target=cpu_bound_task, args=(num_jobs // num_proc,))
+        p.start()
+        procs.append(p)
+
+    for p in procs:
+        p.join()
+    runtime = perf_counter() - start
+
+    print(f"* [{num_proc}] thread runtime: {runtime:.4f} sec")
 
 
 def main():
 
     test_threads = [1, 2, 4, 8]
+    num_jobs = 10000000
 
-    print(f"---- START CPU BOUND TEST ----")
+    print(f"---- START MULTI THREAD TEST ----")
+    print(f"CPU BOUND:")
     for num_thread in test_threads:
-        test_cpu_bound(num_thread)
-    print(f"---- END TEST ----\n")
+        test_cpu_bound_thread(num_thread, num_jobs)
 
-    print(f"---- START I/O BOUND TEST ----")
+    print(f"\nI/O BOUND:")
     for num_thread in test_threads:
-        test_io_bound(num_thread)
-    print(f"---- END TEST ----")
+        test_io_bound_thread(num_thread)
+
+    print(f"\n---- START MULTI PROCESS TEST ----")
+    print(f"CPU BOUND:")
+    for num_thread in test_threads:
+        test_cpu_bound_process(num_thread, num_jobs)
+
+    print(f"\nI/O BOUND:")
+    for num_thread in test_threads:
+        test_io_bound_process(num_thread)
 
 
 if __name__ == "__main__":
